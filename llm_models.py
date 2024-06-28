d39cdb7d0a5b270f0afe9e1efa3a618b6d35064c
MOHAMMAD ALBARHAM <mohammedbrham98@gmail.com>
2024-06-28 23:57:13 +0400

    Revert "formatting python files"

--- llm_models.py
+++ llm_models.py
@@ -4,12 +4,10 @@ from pydantic import BaseModel
 from typing import List
 from image_generator import get_image
 
-
 class StepByStepAIResponse(BaseModel):
     title: str
     story_segments: List[str]
     image_prompts: List[str]
-
 
 class GetTranslation(BaseModel):
     translated_text: List[str]
@@ -19,14 +17,14 @@ client = OpenAI(api_key="key_here")
 
 
 def generate_story(k, prompt):
-    """Generate a story with k segments and initial prompt"""
+        """ Generate a story with k segments and initial prompt"""
 
-    response = client.chat.completions.create(
+        response = client.chat.completions.create(
         model="gpt-4-turbo-preview",
         messages=[
             {
-                "role": "system",
-                "content": f"""
+            "role": "system",
+            "content": f"""
             Your expertise lies in weaving captivating narratives for children, complemented by images that vividly bring each tale to life. Embark on a creative endeavor to construct a story segmented into {k} distinct chapters, each a cornerstone of an enchanting journey for a young audience.
             The input prompt will be on Arabic, but the output must be in English.
 
@@ -54,7 +52,7 @@ def generate_story(k, prompt):
 
             Through your skill in melding textual and visual storytelling, you will breathe life into this magical tale, offering young readers a journey to remember through both prose and illustration.
 
-            """,
+            """
             },
         ],
         functions=[
@@ -64,31 +62,30 @@ def generate_story(k, prompt):
                 "parameters": StepByStepAIResponse.model_json_schema(),
             }
         ],
-        function_call={
-            "name": "get_story_segments_and_image_prompts"
-        },  # Corrected to match the defined function name
+        function_call={"name": "get_story_segments_and_image_prompts"},  # Corrected to match the defined function name
         temperature=1,
         max_tokens=1000,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0,
-    )
+        presence_penalty=0
+        )
 
-    output = json.loads(response.choices[0].message.function_call.arguments)
+        output = json.loads(response.choices[0].message.function_call.arguments)
 
-    sbs = StepByStepAIResponse(**output)
+        sbs = StepByStepAIResponse(**output)
 
-    return sbs
-
-
+        return sbs
+    
 def get_Arabic_translation(story_segments):
-
-    response = client.chat.completions.create(
+        
+        response = client.chat.completions.create(
         model="gpt-4-turbo-preview",
         messages=[
             {
-                "role": "system",
-                "content": f"""
+            "role": "system",
+            "content": 
+            
+            f"""
             You are an expert translator of text from English to Arabic.
 
             On the following, you can find the input text that you need to translate to Arabic: 
@@ -96,7 +93,7 @@ def get_Arabic_translation(story_segments):
 
             Translate it from English to Arabic.
 
-            """,
+            """
             },
         ],
         functions=[
@@ -106,22 +103,19 @@ def get_Arabic_translation(story_segments):
                 "parameters": GetTranslation.model_json_schema(),
             }
         ],
-        function_call={
-            "name": "translate_text_from_english_to_arabic"
-        },  # Corrected to match the defined function name
+        function_call={"name": "translate_text_from_english_to_arabic"},  # Corrected to match the defined function name
         temperature=1,
         max_tokens=1000,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0,
-    )
+        presence_penalty=0
+        )
 
-    output = json.loads(response.choices[0].message.function_call.arguments)
+        output = json.loads(response.choices[0].message.function_call.arguments)
 
-    sbs = GetTranslation(**output)
+        sbs = GetTranslation(**output)
 
-    return sbs
-
+        return sbs
 
 def get_text_image_pairs(k, prompt):
 
